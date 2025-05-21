449fc47c66169a671a7065fecbef3335acaf3cf5
GabrielWojcik <gabrielwojcikv@gmail.com>
2025-05-21 13:14:52 -0300

    feat - new navbar component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,43 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+import Img1 from "../images/flower/vaso-1.png";
+import Img2 from "../images/flower/vaso-2.png";
+import Img3 from "../images/flower/vaso-3.png";
+import Image from "next/image";
+export function Banner() {
+  return (
+    <div className="bg-[#e4f1dc] text-[#2f5e3c] flex flex-col items-center h-96 p-4 gap-2 md:flex-row">
+      <div className="w-full md:w-1/2">
+        <h1 className="text-5xl mb-2">
+          Traga a natureza <br /> para dentro de casa
+        </h1>
+        <p className="font-medium mb-8">
+          Descubra nossa coleção de plantas, suculentas, <br /> cactos e muito
+          mais para transformar seu espaço em <br /> um oásis verde.
+        </p>
+        <div className="flex gap-4">
+          <button className="bg-[#3c6f4d] text-white py-2 px-8 rounded font-bold flex items-center gap-4 cursor-pointer hover:bg-green-700">
+            Comprar agora <FaArrowRightLong />
+          </button>
+          <button className="bg-white text-[#3c6f4d] py-2 px-8 border-[#3c6f4d] border rounded cursor-pointer hover:bg-[#3c6f4d] hover:text-white">
+            Ver catálogo
+          </button>
+        </div>
+      </div>
+      <div className=" hidden  items-center justify-end h-96 w-full md:flex">
+        <div className=" items-end flex pb-14 -mr-6 h-full">
+          <div className="w-[60px] h-[60px] rounded-full overflow-hidden">
+            <Image src={Img2} alt="" className="object-cover" />
+          </div>
+        </div>
+        <div className="w-[280px] h-[280px] relative rounded-full overflow-hidden">
+          <Image src={Img1} alt="" fill className="object-cover" />
+        </div>
+        <div className=" items-start flex pb-14 h-full">
+          <div className="w-[180px] h-[180px] relative rounded-full overflow-hidden">
+            <Image src={Img3} alt="" fill className="object-cover" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,42 @@
+import { FiShoppingBag } from "react-icons/fi";
+import { IoIosSearch } from "react-icons/io";
+import Logo from "../../images/logo-2-meu-jardim.png";
+import Image from "next/image";
+
+export function NavBar() {
+  return (
+    <div className="flex text-green-800 items-center justify-between mx-8">
+      <div>
+        <Image
+          className="rounded-full"
+          src={Logo}
+          alt=""
+          width={120}
+          height={120}
+        />
+      </div>
+      <div>
+        <ul className="flex gap-4">
+          <li>Início</li>
+          <li>Plantas</li>
+          <li>Vasos</li>
+          <li>Sobre</li>
+          <li>Contato</li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="cursor-pointer">
+          <IoIosSearch size={20} color="#3F6212" />
+        </div>
+        <div className="border p-2 border-lime-200 relative rounded-sm">
+          <div className="w-6 h-6 flex items-center justify-center text-white text-center absolute font-bold -top-2 -right-2 bg-lime-800 rounded-full z-10">
+            0
+          </div>
+          <div className="m-1 cursor-pointer">
+            <FiShoppingBag color="#3F6212" size={20} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
